c5b2be4d01df0b2aa97e493833b7dbca1237c285
Boonsom W <bworrawa@student.42.fr>
2025-03-04 15:02:16 +0700

    Adding Utility functions, fixing some of the response setters

--- HttpResponse.cpp
+++ HttpResponse.cpp
@@ -3,29 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   HttpResponse.cpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nusamank <nusamank@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bworrawa <bworrawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 12:56:59 by bworrawa          #+#    #+#             */
-/*   Updated: 2025/03/04 13:51:33 by bworrawa         ###   ########.fr       */
+/*   Updated: 2025/03/04 14:46:39 by bworrawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HttpResponse.hpp"
-
-
-static std::string trim(const std::string& str) {
-    size_t start = 0;
-    while (start < str.length() && std::isspace(str[start])) {
-        ++start;
-    }
-
-    size_t end = str.length();
-    while (end > start && std::isspace(str[end - 1])) {
-        --end;
-    }
-
-    return str.substr(start, end - start);
-}
 
 
 HttpResponse::HttpResponse():status(200)
@@ -57,7 +42,7 @@ std::string HttpResponse::getHeader(std::string name) const
 		return "";
 }
 
-bool HttpResponse::setHeader(std::string name, std::string value , bool overwriteExisting=false)
+bool HttpResponse::setHeader(std::string name, std::string value , bool overwriteExisting)
 {
 	std::map<std::string, std::string>::const_iterator pos = headers.find(name);
 	if (pos != headers.end() && !overwriteExisting)
@@ -85,15 +70,6 @@ std::map<std::string, std::string> 	&HttpResponse::getHeaders(void)
 	return headers;
 }
 
-bool HttpResponse::setStatus(int statusCode)
-{
-	if (statusCode > 0)
-	{
-		status = statusCode;
-		return true;
-	}
-	return false;
-}
 
 int	HttpResponse::getStatus()
 {
@@ -105,15 +81,6 @@ std::string	HttpResponse::getBody() const
 	return body;
 }
 
-bool HttpResponse::setBody(std::string body)
-{
-	if (body != "")
-	{
-		this->body = body;
-		return true;
-	}
-	return false;
-}
 
 std::string HttpResponse::getStatusText(int statusCode)
 {
@@ -162,13 +129,13 @@ std::string	HttpResponse::getDefaultErrorPage(int statusCode)
 	
 	std::ifstream file("errorPages/errorPage.html");
 	if (!file.is_open())
-        return "<html><body><h1>Error " + std::to_string(statusCode) + "</h1><p>" + errorText + "</p></body></html>";
+        return "<html><body><h1>Error " + Util::toString(statusCode) + "</h1><p>" + errorText + "</p></body></html>";
 	std::stringstream buffer;
 	buffer << file.rdbuf();
 	std::string errorPage = buffer.str();
 
 	std::map<std::string, std::string> replacements;
-	replacements["{{statusCode}}"] = std::to_string(statusCode);
+	replacements["{{statusCode}}"] = Util::toString(statusCode);
     replacements["{{errorText}}"] = errorText;
 
 	std::map<std::string, std::string>::iterator it;
@@ -185,13 +152,11 @@ std::string	HttpResponse::getDefaultErrorPage(int statusCode)
 std::string HttpResponse::serialize()
 {
 	std::ostringstream  	oss;
-	std::stringstream 		ss;
 	oss << "HTTP/1.1 " << status << " " << getStatusText(status) << "\r\n";
-	ss << body.size();
-	setHeader("Content-Length", ss.str(), true);
+	setHeader("Content-Length", Util::toString(body.size()) , true);
 
 	for (std::map<std::string,std::string>::const_iterator it = headers.begin(); it != headers.end(); ++it)
-		oss << trim(it->first) << ": " << it->second << "\r\n";
+		oss << Util::trim(it->first) << ": " << it->second << "\r\n";
 	// single set of \r\n since the header already sent the first set
 	oss << "\r\n" << body;
 
@@ -231,10 +196,3 @@ bool HttpResponse::setBody(std::string bodyString)
 	return (true);
 }
 
-
-bool HttpResponse::setHeader(std::string name, std::string value , bool overwriteExisting)
-{
-	(void)overwriteExisting;
-	headers[name] = value;
-	return (true);
-}
---
+++ Util.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Util.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bworrawa <bworrawa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 14:37:19 by bworrawa          #+#    #+#             */
+/*   Updated: 2025/03/04 14:47:19 by bworrawa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include 	"Util.hpp"
+#include 	<iostream>
+#include 	<sstream>
+
+std::string Util::trim(std::string& str)
+{
+    size_t start = 0;
+    while (start < str.length() && std::isspace(str[start])) {
+        ++start;
+    }
+
+    size_t end = str.length();
+    while (end > start && std::isspace(str[end - 1])) {
+        --end;
+    }
+
+    return str.substr(start, end - start);
+}
+
+std::string Util::trim(std::string str)
+{
+    size_t start = 0;
+    while (start < str.length() && std::isspace(str[start])) {
+        ++start;
+    }
+
+    size_t end = str.length();
+    while (end > start && std::isspace(str[end - 1])) {
+        --end;
+    }
+
+    return str.substr(start, end - start);
+}
+std::string	Util::toString(int i)
+{
+	std::stringstream 		ss;
+	ss << i;
+	return (ss.str());
+}
+
+std::string	Util::toString(size_t i)
+{
+	std::stringstream 		ss;
+	ss << i;
+	return (ss.str());
+
+}
