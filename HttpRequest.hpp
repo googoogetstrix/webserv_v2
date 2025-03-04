3cd2cf8c4878318b8059b4336e25f86f1bfc0d3c
Nudchada Samankatiwat <nusamank@z1t6c3.42bangkok.com>
2025-03-04 16:28:05 +0700

    getter setter definition

--- HttpRequest.hpp
+++ HttpRequest.hpp
@@ -3,16 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   HttpRequest.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bworrawa <bworrawa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nusamank <nusamank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 10:10:12 by bworrawa          #+#    #+#             */
-/*   Updated: 2025/03/04 15:47:44 by bworrawa         ###   ########.fr       */
+/*   Updated: 2025/03/04 16:24:47 by nusamank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef 	HTTP_REQUEST_HPP
 # define	HTTP_REQUEST_HPP
+# include	<map>
+# include	<string>
 # include   "ServerConfig.hpp"
+# include	"HttpResponse.hpp"
 
 class ServerConfig;
 
@@ -40,6 +43,23 @@ class HttpRequest
 		HttpRequest(HttpRequest const &other);
 		HttpRequest &operator=(HttpRequest const &other);
 		~HttpRequest();
+
+		const std::string &getMethod() const;
+		const std::string &getPath() const;
+		const std::string &getRawPath() const;
+		const std::map<std::string, std::string> &getHeader() const;
+		size_t getContentLength() const;
+		const std::map<std::string, std::string> &getqueryStrings() const;
+		const std::string &getBody() const;
+
+		bool setMethod(std::string methodStr);
+		bool setPath(std::string pathStr);
+		bool setRawPath(std::string rawPathStr);
+		bool setHeader(std::string name, std::string value , bool overwriteExisting=false);
+		bool setContentLength(size_t contentLengthVal);
+		bool setQueryString(std::string key, std::string value);
+		bool setBody(std::string bodyStr);
+
 
 		// &response ==> for setting value in case on error
 		// ServerConfig
